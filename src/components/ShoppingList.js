68e5d8d7f0a837903b8ca196bdefeec09079cc33
Justin <justin3459@hotmail.com>
2023-06-13 14:00:28 -0400

    Add filter/map functionality to shoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState}from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [setCat, isSetCat] = useState("All")
+
+  const handleSetCat = (e) =>{isSetCat(e.target.value)}
+
+  const displayCat = items.filter((item) => {
+    if(setCat === "All"){
+      return setCat
+    }
+    else{
+      return item.category === setCat
+    }
+  
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSetCat}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayCat.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
